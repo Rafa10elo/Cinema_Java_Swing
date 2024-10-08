d29ea9ccd8b9c9d8041c23dd94d79da0deae665b
Rafa10elo <hel11qen70@gmail.com>
2024-10-08 23:13:15 +0300

    loading page 2nd edition

--- src/LodingPage.java
+++ src/LodingPage.java
@@ -3,91 +3,97 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class LodingPage  extends JPanel {
-        private static final int SIZE = 100;
-        private static final int DOT_RADIUS = 10;
-        private static final int DOT_COUNT = 8;
-        private int angle = 0;
-        private float messageOpacity = 0.0f;
-        private boolean showMessage = false;
+public class LodingPage extends JPanel {
+    private static final int SIZE = 100;
+    private static final int DOT_RADIUS = 10;
+    private static final int DOT_COUNT = 8;
+    private int angle = 0;
+    private float messageOpacity = 0.0f;
+    private boolean showMessage = false;
+    private FadeCompleteListener fadeCompleteListener;
 
-        public LodingPage() {
-
-            Timer spinnerTimer = new Timer(100, new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    if (!showMessage) {
-                        angle += 30;
-                        if (angle >= 360) {
-                            angle = 0;
-                        }
-                        repaint();
-                    }
-                }
-            });
-            spinnerTimer.start();
-
-
-            Timer messageTimer = new Timer(5000, new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    showMessage = true;
-                    ((Timer) e.getSource()).stop();
-                    startMessageFadeIn();
-                }
-            });
-            messageTimer.setRepeats(false);
-            messageTimer.start();
-
-        }
-
-
-        private void startMessageFadeIn() {
-            Timer fadeInTimer = new Timer(100, new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    messageOpacity += 0.05f;
-                    if (messageOpacity >= 1.0f) {
-                        messageOpacity = 1.0f;
-                        ((Timer) e.getSource()).stop();
+    public LodingPage() {
+        Timer spinnerTimer = new Timer(100, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!showMessage) {
+                    angle += 30;
+                    if (angle >= 360) {
+                        angle = 0;
                     }
                     repaint();
                 }
-            });
-            fadeInTimer.start();
-        }
-
-
-        @Override
-        protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            Graphics2D g2d = (Graphics2D) g.create();
-            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-            int centerX = getWidth() / 2;
-            int centerY = getHeight() / 2;
-
-            if (!showMessage) {
-
-                for (int i = 0; i < DOT_COUNT; i++) {
-                    double theta = Math.toRadians(i * 360.0 / DOT_COUNT + angle);
-                    int x = (int) (centerX + Math.cos(theta) * (SIZE / 2 - DOT_RADIUS));
-                    int y = (int) (centerY + Math.sin(theta) * (SIZE / 2 - DOT_RADIUS));
-                    float opacity = (i + 1) / (float) DOT_COUNT;
-                    g2d.setColor(new Color(0, 0, 0, opacity));
-                    g2d.fillOval(x - DOT_RADIUS, y - DOT_RADIUS, DOT_RADIUS * 2, DOT_RADIUS * 2);
-                }
-            } else {
-
-                g2d.setFont(new Font("Arial", Font.BOLD, 36));
-                g2d.setColor(new Color(0, 0, 0, messageOpacity));
-                String welcomeMessage = "Welcome!";
-                FontMetrics fm = g2d.getFontMetrics();
-                int messageWidth = fm.stringWidth(welcomeMessage);
-                g2d.drawString(welcomeMessage, centerX - messageWidth / 2, centerY);
             }
+        });
+        spinnerTimer.start();
 
-            g2d.dispose();
+        Timer messageTimer = new Timer(5000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showMessage = true;
+                ((Timer) e.getSource()).stop();
+                startMessageFadeIn();
+            }
+        });
+        messageTimer.setRepeats(false);
+        messageTimer.start();
+    }
+
+    private void startMessageFadeIn() {
+        Timer fadeInTimer = new Timer(100, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                messageOpacity += 0.05f;
+                if (messageOpacity >= 1.0f) {
+                    messageOpacity = 1.0f;
+                    ((Timer) e.getSource()).stop();
+
+                    // Notify that the fade-in is complete
+                    if (fadeCompleteListener != null) {
+                        fadeCompleteListener.onFadeComplete();
+                    }
+                }
+                repaint();
+            }
+        });
+        fadeInTimer.start();
+    }
+
+    public void setFadeCompleteListener(FadeCompleteListener listener) {
+        this.fadeCompleteListener = listener;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g.create();
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        int centerX = getWidth() / 2;
+        int centerY = getHeight() / 2;
+
+        if (!showMessage) {
+            for (int i = 0; i < DOT_COUNT; i++) {
+                double theta = Math.toRadians(i * 360.0 / DOT_COUNT + angle);
+                int x = (int) (centerX + Math.cos(theta) * (SIZE / 2 - DOT_RADIUS));
+                int y = (int) (centerY + Math.sin(theta) * (SIZE / 2 - DOT_RADIUS));
+                float opacity = (i + 1) / (float) DOT_COUNT;
+                g2d.setColor(new Color(0, 0, 0, opacity));
+                g2d.fillOval(x - DOT_RADIUS, y - DOT_RADIUS, DOT_RADIUS * 2, DOT_RADIUS * 2);
+            }
+        } else {
+            g2d.setFont(new Font("Arial", Font.BOLD, 36));
+            g2d.setColor(new Color(0, 0, 0, messageOpacity));
+            String welcomeMessage = "Welcome!";
+            FontMetrics fm = g2d.getFontMetrics();
+            int messageWidth = fm.stringWidth(welcomeMessage);
+            g2d.drawString(welcomeMessage, centerX - messageWidth / 2, centerY);
         }
 
+        g2d.dispose();
     }
+
+    public interface FadeCompleteListener {
+        void onFadeComplete();
+    }
+}
--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,60 +1,81 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class MyFrame extends JFrame{
+public class MyFrame extends JFrame {
 
-    MyFrame(){
-        String[] genres={"Horror","Action","Romance","Adventure"};
+    private JPanel panel;
+    private JPanel panel1;
+    private JPanel panel2;
+    private JPanel panel3;
+    private LodingPage lodingPage;
+
+    MyFrame() {
+        String[] genres = {"Horror", "Action", "Romance", "Adventure"};
         this.setTitle("Cinema");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLayout(new BorderLayout());
-        this.setSize(500,500);
-        LodingPage lodingPage = new LodingPage();
-        String[] showtime={"Thursday: 06:00PM-08:00PM-10:00PM","Friday: 06:00PM-08:00PM-10:00PM"};
+        this.setSize(500, 500);
 
-        Movie movie1=new Movie(1,genres[1],"NAME1",showtime[0]);
-        Movie movie2=new Movie(2,genres[2],"NAME2",showtime[1]);
-        String[] movies={movie1.name,movie2.name};
-        Movie[] movies1={movie1,movie2};
-        JLabel label=new JLabel("Now In Cinemas!");
+        lodingPage = new LodingPage();
+        String[] showtime = {"Thursday: 06:00PM-08:00PM-10:00PM", "Friday: 06:00PM-08:00PM-10:00PM"};
+
+        Movie movie1 = new Movie(1, genres[1], "NAME1", showtime[0]);
+        Movie movie2 = new Movie(2, genres[2], "NAME2", showtime[1]);
+        String[] movies = {movie1.name, movie2.name};
+
+        JLabel label = new JLabel("Now In Cinemas!");
         label.setBackground(Color.black);
 
-        JList<String>list=new JList<>(movies);
+        JList<String> list = new JList<>(movies);
         list.setVisibleRowCount(2);
-        list.setBackground(Color.red);//i'll add it later,i didn't decide where to put it
+        list.setBackground(Color.red);
 
+        JComboBox<String> comboBox = new JComboBox<>(movies);
 
-        JComboBox<String> comboBox= new JComboBox<>(movies);
-
-        JPanel panel=new JPanel();
-        JPanel panel1=new JPanel();
-        JPanel panel2=new JPanel();
-        JPanel panel3=new JPanel(new FlowLayout());
+        panel = new JPanel();
+        panel1 = new JPanel();
+        panel2 = new JPanel();
+        panel3 = new JPanel(new FlowLayout());
         panel1.setBackground(Color.darkGray);
         panel2.setBackground(Color.darkGray);
         panel3.setBackground(Color.YELLOW);
-        panel1.setPreferredSize(new Dimension(50,50));
-        panel2.setPreferredSize(new Dimension(50,50));
-        panel3.setPreferredSize(new Dimension(50,50));
+        panel1.setPreferredSize(new Dimension(50, 50));
+        panel2.setPreferredSize(new Dimension(50, 50));
+        panel3.setPreferredSize(new Dimension(50, 50));
 
-        panel.setBounds(800,600,100,100);
+        panel.setBounds(800, 600, 100, 100);
         panel.add(comboBox);
         panel.setBackground(Color.black);
         panel.setOpaque(true);
 
-
-        this.add(panel);
-        this.add(panel1,BorderLayout.NORTH);
-        this.add(panel2,BorderLayout.WEST);
+        this.add(panel1, BorderLayout.NORTH);
+        this.add(panel2, BorderLayout.WEST);
         panel3.add(label);
         panel3.add(list);
-        this.add(panel3,BorderLayout.CENTER);
+        this.add(panel3, BorderLayout.CENTER);
+
+        // // ITS TEST THING FOR THE REMOVAL OF THE LOADING PAGE
+        panel.setVisible(false);
+        panel1.setVisible(false);
+        panel2.setVisible(false);
+        panel3.setVisible(false);
         this.add(lodingPage);
 
+        lodingPage.setFadeCompleteListener(() -> {
+
+            this.remove(lodingPage);
+            panel.setVisible(true);
+            panel1.setVisible(true);
+            panel2.setVisible(true);
+            panel3.setVisible(true);
+            this.revalidate();
+            this.repaint();
+        });
 
         this.setVisible(true);
-
     }
 
-
+    public static void main(String[] args) {
+        new MyFrame();
+    }
 }
