4d4fb3f63540e339c8db01051a01e9c68b8c60cb
Karam-Nssr <karamnssr@gmail.com>
2024-10-08 18:05:05 +0300

    first commit

---
+++ src/Movie.java
@@ -0,0 +1,13 @@
+public class Movie {
+    final int id;
+    final String genre;
+    final String name;
+    String ShowTimes;
+    Movie(int id, String genre,String name,String ShowTimes)
+    {
+        this.id=id;
+        this.genre=genre;
+        this.name=name;
+        this.ShowTimes=ShowTimes;
+    }
+}
---
+++ src/MyFrame.java
@@ -0,0 +1,56 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MyFrame extends JFrame{
+    MyFrame(){
+        String[] genres={"Horror","Action","Romance","Adventure"};
+        this.setTitle("Cinema");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLayout(new BorderLayout());
+        this.setSize(500,500);
+        String[] showtime={"Thursday: 06:00PM-08:00PM-10:00PM","Friday: 06:00PM-08:00PM-10:00PM"};
+
+        Movie movie1=new Movie(1,genres[1],"NAME1",showtime[0]);
+        Movie movie2=new Movie(2,genres[2],"NAME2",showtime[1]);
+        String[] movies={movie1.name,movie2.name};
+        Movie[] movies1={movie1,movie2};
+        JLabel label=new JLabel("Now In Cinemas!");
+        label.setBackground(Color.black);
+
+        JList<String>list=new JList<>(movies);
+        list.setVisibleRowCount(2);
+        list.setBackground(Color.red);//i'll add it later,i didn't decide where to put it
+
+
+        JComboBox<String> comboBox= new JComboBox<>(movies);
+
+        JPanel panel=new JPanel();
+        JPanel panel1=new JPanel();
+        JPanel panel2=new JPanel();
+        JPanel panel3=new JPanel(new FlowLayout());
+        panel1.setBackground(Color.darkGray);
+        panel2.setBackground(Color.darkGray);
+        panel3.setBackground(Color.YELLOW);
+        panel1.setPreferredSize(new Dimension(50,50));
+        panel2.setPreferredSize(new Dimension(50,50));
+        panel3.setPreferredSize(new Dimension(50,50));
+
+        panel.setBounds(800,600,100,100);
+        panel.add(comboBox);
+        panel.setBackground(Color.black);
+        panel.setOpaque(true);
+
+
+        this.add(panel);
+        this.add(panel1,BorderLayout.NORTH);
+        this.add(panel2,BorderLayout.WEST);
+        panel3.add(label);
+        panel3.add(list);
+        this.add(panel3,BorderLayout.CENTER);
+
+        this.setVisible(true);
+
+    }
+
+
+}
