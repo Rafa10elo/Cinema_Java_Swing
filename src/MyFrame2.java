a9bfe075830fb1f04c533a4352107388ce8028fb
Rafa10elo <hel11qen70@gmail.com>
2024-10-11 17:43:39 +0300

    fast fix

--- src/MyFrame2.java
+++ src/MyFrame2.java
@@ -12,7 +12,7 @@ public class MyFrame2 extends JFrame {
 
     public MyFrame2() {
         setTitle("Movies");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         setSize(800, 600);
         setBackground(Color.BLACK);
 
